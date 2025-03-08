708928a6668640d3a811c248c2f618aee46da515
KevinDev <kev.trujillo@udla.edu.co>
2025-03-07 19:05:21 -0500

    migrando a nuevo diseño 2.0

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes=[{
   children:[
     {path:'sign-in', component:LoginPageComponent},
     {path:'recuperar', component:RecuperaContrasenaComponent},
-    {path:'**',redirectTo:'sign-in'}
+    {path:'**',redirectTo:'recuperar'}
   ]
 }]
 
