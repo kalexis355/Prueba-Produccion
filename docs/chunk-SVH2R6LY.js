76e387c24ba12f612718434dae1a978f37c6c6a5
KevinDev <kev.trujillo@udla.edu.co>
2025-05-07 15:00:09 -0500

    indicie electronico 3.0

---
+++ docs/chunk-SVH2R6LY.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-RQDSKBAD.js";import"./chunk-A43TDCY5.js";export default a();
--- src/app/dashboard/components/indice-electronico/indice-electronico.component.ts
+++ src/app/dashboard/components/indice-electronico/indice-electronico.component.ts
@@ -1,8 +1,13 @@
-import { Component, inject, Inject, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, Inject, OnInit, ViewChild } from '@angular/core';
 import { CarpetasPadre, CarpetaBase, ContenidoCarpetaResponse } from '../../interfaces/carpeta.interface';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { GestionCarpetasService } from '../../services/gestionCarpetas.service';
 import { IndexDbService } from '../../services/indexdb.service';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-indice-electronico',
@@ -16,6 +21,8 @@ export class IndiceElectronicoComponent implements OnInit{
 
   carpetaActual!: CarpetasPadre | CarpetaBase;
   contenidoCarpeta!: ContenidoCarpetaResponse;
+
+  @ViewChild('tablaPDF', { static: false }) tablaPDF!: ElementRef;
 
   constructor(
     public dialogRef: MatDialogRef<IndiceElectronicoComponent>,
@@ -53,6 +60,39 @@ export class IndiceElectronicoComponent implements OnInit{
 
       }
     })
+  }
+
+  descargarPdf(): void {
+   // Da tiempo para asegurar que Angular renderice todo
+   setTimeout(() => {
+    const DATA = this.tablaPDF.nativeElement;
+
+    html2canvas(DATA, {
+      scale: 2,
+      useCORS: true,
+      allowTaint: true
+    }).then(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('l', 'mm', 'a4'); // Apaisado
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.save('indice-electronico.pdf');
+    }).catch(error => {
+      console.error('Error al generar el PDF:', error);
+    });
+  }, 100); // Puede ajustarse según el tiempo de carga de datosM esté listo
+  }
+
+  descargarExcel(){
+    const tabla = this.tablaPDF.nativeElement.querySelector('table') as HTMLTableElement;
+    const ws = XLSX.utils.table_to_sheet(tabla);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Documentos');
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    FileSaver.saveAs(blob, 'indice-electronico.xlsx');
   }
 
 
